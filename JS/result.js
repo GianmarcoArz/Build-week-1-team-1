3d814329a8e57ec9625404400800d83ac7e5f0ad
Federico Peralta <federico.peralta91@gmail.com>
2024-09-06 13:14:27 +0200

    finish question summery

--- JS/result.js
+++ JS/result.js
@@ -2,6 +2,36 @@ const score = sessionStorage.getItem("score");
 console.log(score);
 const questionLength = sessionStorage.getItem("totalQuestions");
 const userAnswers = JSON.parse(sessionStorage.getItem("userAnswers"));
+console.log(userAnswers);
+for (let i = 0; i < userAnswers.length; i++) {
+  const answer = userAnswers[i];
+  const answerContainer = document.getElementById("questions-list");
+  const answerDifficult = document.createElement("h4");
+  answerDifficult.innerText = answer.difficulty;
+  answerContainer.appendChild(answerDifficult);
+  const question = document.createElement("h3");
+  question.innerText = answer.question;
+  answerContainer.appendChild(question);
+  const questionUl = document.createElement("ul");
+
+  for (let j = 0; j < answer.answers.length; j++) {
+    const li = document.createElement("li");
+    li.innerText = answer.answers[j];
+
+    if (li.innerText === answer.userAnswers) {
+      li.classList.add("selected");
+      if (li.innerText === answer.correctAnswers) {
+        li.classList.add("correct");
+      } else {
+        li.classList.add("incorrect");
+      }
+    } else if (li.innerText === answer.correctAnswers) {
+      li.classList.add("correct");
+    }
+    questionUl.appendChild(li);
+  }
+  answerContainer.appendChild(questionUl);
+}
 
 let correctPerc = document.getElementById("correct-perc");
 let wrongPerc = document.getElementById("wrong-perc");
